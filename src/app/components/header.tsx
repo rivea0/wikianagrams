0a60c9fb8f5cba6c3db3ee272354049c106cbdad
rivea0 <58330360+rivea0@users.noreply.github.com>
2023-09-17 18:21:14 +0300

    Remove hover color

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -31,7 +31,7 @@ export default function Header() {
             {/* <Link href="/about" className="text-slate-900 block pl-3 pr-4 py-2 rounded">About</Link> */}
             <div className="flex md:hidden md:order-2" data-testid="mobile-menu">
               <button data-collapse-toggle="mobile-menu-3" type="button"
-                className="md:hidden text-gray-400 hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-san-marino-300 rounded-lg inline-flex items-center justify-center"
+                className="md:hidden text-gray-400 focus:outline-none focus:ring-2 focus:ring-san-marino-300 rounded-lg inline-flex items-center justify-center"
                 aria-controls="mobile-menu-3" aria-expanded="false" onClick={handle} data-testid="menu-btn">
                 <span className="sr-only">Open main menu</span>
                 <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
