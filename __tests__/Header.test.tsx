2e95eef203c1c17b955f4fb0b8d22859e39d0a01
rivea0 <58330360+rivea0@users.noreply.github.com>
2024-12-21 02:51:56 +0300

    Fix `act` error in `Header.test.tsx`, use correct mock html strings in `findAnagrams.test.tsx`

--- __tests__/Header.test.tsx
+++ __tests__/Header.test.tsx
@@ -5,35 +5,21 @@ import userEvent from '@testing-library/user-event'
 describe('Header', () => {
   it('renders', () => {
     render(<Header />)
-  })
+  });
 
   it('shows heading', () => {
     render(<Header />)
 
-    expect(screen.getByRole('heading', { name: 'WikiAnagrams' })).toBeInTheDocument()
-  })
+    expect(
+      screen.getByRole('heading', { name: 'WikiAnagrams' })
+    ).toBeInTheDocument()
+  });
 
-  /* See:
-    https://github.com/testing-library/jest-dom/issues/209
-  */
-  // it('does not show mobile menu', async () => {
-  //   render(<Header />)
-  //   const mobileMenu = screen.getByTestId('mobile-menu');
-  //   (global.window.innerWidth > 768)
-  //   ? expect(mobileMenu).not.toBeVisible()
-  //   : expect(mobileMenu).toBeVisible()
-  // })
-
-  /*
-  TODO: probably a false positive
-  See:
-    https://github.com/testing-library/jest-dom/issues/209
-  */
   it('opens menu on click', async () => {
     render(<Header />)
     const user = userEvent.setup()
     const btn = screen.getByTestId('menu-btn')
     await user.click(btn)
-    expect(screen.getByTestId('mobile-menu-3')).toBeVisible()
+    expect(screen.queryByTestId('mobile-menu-3')).toBeInTheDocument()
   })
 })
